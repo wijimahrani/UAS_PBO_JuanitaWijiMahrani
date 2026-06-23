fa34df399078bd27676460d1d6f1d795403a4e0f
Juanita Wiji M <wijimahrani08@gmail.com>
2026-06-23 10:46:42 +0700

    [Tahap 4] Menambahkan method select di class KaryawanKontrak

--- classes/KaryawanKontrak.php
+++ classes/KaryawanKontrak.php
@@ -1,36 +1,45 @@
 <?php
 
 require_once 'Karyawan.php';
+require_once '../database/Koneksi.php';
 
 class KaryawanKontrak extends Karyawan
 {
     protected $durasiKontrakBulan;
     protected $agensiPenyalur;
 
-    public function __construct(
-        $id_karyawan,
-        $nama_karyawan,
-        $departemen,
-        $hariKerjaMasuk,
-        $gajiDasarPerHari,
-        $durasiKontrakBulan,
-        $agensiPenyalur
-    ) {
+    public static function getDataKontrak()
+    {
+        $db = new Koneksi();
+        $conn = $db->getConnection();
+
+        $sql = "SELECT * FROM tabel_karyawan
+                WHERE jenis_karyawan='Kontrak'";
+
+        return $conn->query($sql);
+    }
+
+    public function __construct($data)
+    {
         parent::__construct(
-            $id_karyawan,
-            $nama_karyawan,
-            $departemen,
-            $hariKerjaMasuk,
-            $gajiDasarPerHari
+            $data['id_karyawan'],
+            $data['nama_karyawan'],
+            $data['departemen'],
+            $data['hari_kerja_masuk'],
+            $data['gaji_dasar_per_hari']
         );
 
-        $this->durasiKontrakBulan = $durasiKontrakBulan;
-        $this->agensiPenyalur = $agensiPenyalur;
+        $this->durasiKontrakBulan =
+            $data['durasi_kontrak_bulan'];
+
+        $this->agensiPenyalur =
+            $data['agensi_penyalur'];
     }
 
     public function hitungGajiBersih()
     {
-        return $this->hariKerjaMasuk * $this->gajiDasarPerHari;
+        return $this->hariKerjaMasuk *
+               $this->gajiDasarPerHari;
     }
 
     public function tampilkanProfileKaryawan()
